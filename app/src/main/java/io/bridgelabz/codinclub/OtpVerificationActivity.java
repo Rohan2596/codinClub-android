1b2821587cc0d3be917851af9b8ee94875c60ef8
Rohan2596 <rohankadam662@gmail.com>
2021-01-28 10:56:26 +0530

    WIP:Otp Verification Activity & Layout.

---
+++ app/src/main/java/io/bridgelabz/codinclub/OtpVerificationActivity.java
@@ -0,0 +1,14 @@
+package io.bridgelabz.codinclub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtpVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+    }
+}
