fd2162119646d6cb49906a6956d9285a12cc132e
Rohan2596 <rohankadam662@gmail.com>
2021-02-05 18:08:17 +0530

    WIP:api integration for codin Club.

--- app/src/main/java/io/bridgelabz/codinclub/activities/api/ApiClient.java
+++ app/src/main/java/io/bridgelabz/codinclub/activities/api/ApiClient.java
@@ -1,4 +1,21 @@
 package io.bridgelabz.codinclub.activities.api;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiClient {
+
+    public static String BASE_URL="";
+    private static Retrofit retrofit;
+
+    public static Retrofit getClient(){
+
+        if(retrofit==null){
+            retrofit=new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
--- app/src/main/java/io/bridgelabz/codinclub/activities/api/UserService.java
+++ app/src/main/java/io/bridgelabz/codinclub/activities/api/UserService.java
@@ -1,4 +1,10 @@
 package io.bridgelabz.codinclub.activities.api;
 
+import retrofit2.Call;
+import retrofit2.http.POST;
+
 public interface UserService {
+
+    @POST("register")
+    Call<Object>addUser();
 }
